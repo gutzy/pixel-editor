2d02ef10ca446240dfa0adf43cf22101809b79c5
Dan Fessler <fezzinate@gmail.com>
2021-03-03 04:32:28 -0800

    .

--- src/index.js
+++ src/index.js
@@ -18,9 +18,6 @@ import Vue from "vue/dist/vue";
 import "./style.scss";
 import AppManager from "./classes/AppManager";
 import LayersPanel from "./components/LayersPanel";
-import Toolbox from "./components/Toolbox";
-import ColorPalette from "./components/ColorPalette";
-import MenuBar from "./components/MenuBar";
 
 import { Component, litable, html, render } from "../litable";
 import LitMenuBar from "./lit-components/MenuBar";
@@ -67,29 +64,3 @@ class App extends Component {
 const litableApp = litable(App);
 
 render(litableApp(), document.getElementById("app"));
-
-// const vm = new Vue({
-
-// 		async mounted() {
-// 			AppManager.init(this.$refs.canvas);
-// 		},
-
-// 		components : { LayersPanel, Toolbox, ColorPalette, MenuBar },
-
-// 		template : `
-//             <div class="app">
-//             	<div class="header">
-//             		<menu-bar />
-// 				</div>
-// 				<div class="main-content">
-// 					<toolbox />
-// 	            	<div class="canvas-wrapper">
-//     	        		<canvas id="main-canvas" ref="canvas"></canvas>
-// 					</div>
-// 					<color-palette />
-//             		<layers-panel />
-// 				</div>
-// 		    </div>
-// 				`
-// 	},
-// ).$mount('#app');
